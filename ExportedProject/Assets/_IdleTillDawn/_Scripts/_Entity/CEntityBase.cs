d5caa1f5f0c5a3fb91461ea7143fc15667549f46
JiRungYeeOwO <hosik9398@gmail.com>
2026-03-19 18:49:40 +0900

    기능 추가 : 플레이어 작동
    피격 가능 오브젝트 인터페이스 추가, 캐릭터 데이터 SO 추가, 플레이어 컨트롤러 추가, 인풋 핸들러에서 이동 로직 분리 후 플레이어 컨트롤러로 이동
    
    필요없는 Resource폴더 삭제 후 _Resource 폴더명 변경

---
+++ ExportedProject/Assets/_IdleTillDawn/_Scripts/_Entity/CEntityBase.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public abstract class CEntityBase : MonoBehaviour, IDamageable
+{
+    #region 인스펙터
+    [Header("공통 스탯")]
+    [SerializeField] protected float _currentHealth;
+    [SerializeField] protected float _moveSpeed;
+    #endregion
+
+    #region 내부 변수
+    public float CurrentHealth => _currentHealth;
+
+    protected Rigidbody2D _rb;
+    #endregion
+
+    protected virtual void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        HandleMovement();
+        HandleAttack();
+    }
+
+    protected abstract void HandleMovement();
+    protected abstract void HandleAttack();
+
+    public virtual void TakeDamage(float damage)
+    {
+        _currentHealth -= damage;
+        Debug.Log($"[{gameObject.name}] [데미지 : {damage}, 현재 체력 : {_currentHealth}]");
+
+        if (_currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+        Debug.Log($"{gameObject.name} 사망");
+        Destroy(gameObject);
+    }
+}
---
+++ ExportedProject/Assets/_IdleTillDawn/_Scripts/_Entity/Player/CPlayerDataSO.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "2D(SO)/Data/Player Data", fileName = "PlayerDataSO_")]
+public class CPlayerDataSO : ScriptableObject
+{
+	#region 인스펙터
+	[Header("기본 정보")]
+	[SerializeField] private string _characterName = "Character Name";
+	[SerializeField] private string _description = "Character Description";
+	[SerializeField] private Sprite _characterPortrait = null;
+
+	[Header("캐릭터 스탯")]
+	[SerializeField] private float _maxHealth = 50f;
+	[SerializeField] private float _maxMana = 20f;
+	[SerializeField] private float _defaultDamage = 10f;
+	[SerializeField] private float _attackSpeed = 1.5f;
+	[SerializeField] private float _healthRegen = 1f;
+	[SerializeField] private float _manaRegen = 4f;
+	[SerializeField] private float _moveSpeed = 3f;
+	[SerializeField] private float _expMultiplier = 1f;
+
+	[Header("소환할 오브젝트")]
+	[SerializeField] GameObject _prefab;
+	#endregion
+
+	#region 내부 변수
+	public string CharacterName => _characterName;
+	public string Description => _description;
+	public Sprite CharacterPortrait => _characterPortrait;
+
+	public float MaxHealth => _maxHealth;
+	public float MaxMana => _maxMana;
+	public float DefaultDamage => _defaultDamage;
+	public float AttackSpeed => _attackSpeed;
+	public float HealthRegen => _healthRegen;
+	public float ManaRegen => _manaRegen;
+	public float MoveSpeed => _moveSpeed;
+	public float ExpMultiplier => _expMultiplier;
+
+	public GameObject Prefab => _prefab;
+	#endregion
+}
