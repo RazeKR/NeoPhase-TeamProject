7ffb0cbcab6ce8b199149cc169a03acacb3472ee
JiRungYeeOwO <hosik9398@gmail.com>
2026-03-20 18:48:45 +0900

    기능 추가 : 적 / 보스, 자폭 몬스터
    공통 클래스 추가, 자폭 몬스터 애니메이션, 자폭 기능, 코드 리팩토링

--- ExportedProject/Assets/_IdleTillDawn/_Scripts/_Entity/CEntityBase.cs
+++ ExportedProject/Assets/_IdleTillDawn/_Scripts/_Entity/CEntityBase.cs
@@ -1,31 +1,65 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public abstract class CEntityBase : MonoBehaviour, IDamageable
 {
     #region 인스펙터
     [Header("공통 스탯")]
+    [SerializeField] protected float _maxHealth;
     [SerializeField] protected float _currentHealth;
     [SerializeField] protected float _moveSpeed;
+
+    [Header("타겟 탐지 설정")]
+    [SerializeField] protected float _scanRadius = 10f;
+    [SerializeField] protected LayerMask _targetLayer;
+    [SerializeField] protected float _scanInterval = 0.2f;
+
+    [Header("엔티티 정보")]
+    [SerializeField] protected string _entityName;
+    [SerializeField] protected string _description;
+
+    [Header("물리 설정")]
+    [SerializeField] protected float _defaultGravityScale = 0f;
     #endregion
 
     #region 내부 변수
     public float CurrentHealth => _currentHealth;
+    public string EntityName => _entityName;
 
     protected Rigidbody2D _rb;
+    protected Transform _currentTarget;
+    protected float _scanTimer = 0f;
     #endregion
 
     protected virtual void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+
+        _rb.gravityScale = _defaultGravityScale;
+        _rb.freezeRotation = true;
     }
 
     protected virtual void FixedUpdate()
     {
+        FindNearestTarget();
+
         HandleMovement();
         HandleAttack();
     }
 
+    protected virtual void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _scanRadius);
+    }
+
+    /// <summary>
+    /// 움직임 제어
+    /// </summary>
     protected abstract void HandleMovement();
+    /// <summary>
+    /// 공격 제어
+    /// </summary>
     protected abstract void HandleAttack();
 
     public virtual void TakeDamage(float damage)
@@ -43,5 +77,50 @@ public abstract class CEntityBase : MonoBehaviour, IDamageable
     {
         Debug.Log($"{gameObject.name} 사망");
         Destroy(gameObject);
+    }
+
+    /// <summary>
+    /// 가장 가까운 적을 찾는 메서드
+    /// </summary>
+    protected void FindNearestTarget()
+    {
+        _scanTimer += Time.fixedDeltaTime;
+
+        if (_scanTimer < _scanInterval) return;
+        _scanTimer = 0f;
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, _scanRadius, _targetLayer);
+
+        float shortestDistance = Mathf.Infinity;
+        Transform nearestEnemy = null;
+
+        foreach (Collider2D collider in colliders)
+        {
+            float distance = Vector2.Distance(transform.position, collider.transform.position);
+
+            if (distance < shortestDistance)
+            {
+                shortestDistance = distance;
+                nearestEnemy = collider.transform;
+            }
+        }
+
+        _currentTarget = nearestEnemy;
+    }
+
+    /// <summary>
+    /// 캐릭터 좌우 반전
+    /// </summary>
+    /// <param name="velocityX"></param>
+    protected void FlipCharacter(float velocityX)
+    {
+        if (velocityX > 0.001f)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (velocityX < -0.001f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
     }
 }
--- ExportedProject/Assets/_IdleTillDawn/_Scripts/_Entity/Player/CPlayerDataSO.cs
+++ ExportedProject/Assets/_IdleTillDawn/_Scripts/_Entity/Player/CPlayerDataSO.cs
@@ -10,14 +10,14 @@ public class CPlayerDataSO : ScriptableObject
 	[SerializeField] private Sprite _characterPortrait = null;
 
 	[Header("캐릭터 스탯")]
-	[SerializeField] private float _maxHealth = 50f;
-	[SerializeField] private float _maxMana = 20f;
-	[SerializeField] private float _defaultDamage = 10f;
-	[SerializeField] private float _attackSpeed = 1.5f;
-	[SerializeField] private float _healthRegen = 1f;
-	[SerializeField] private float _manaRegen = 4f;
-	[SerializeField] private float _moveSpeed = 3f;
-	[SerializeField] private float _expMultiplier = 1f;
+	[SerializeField] private float _baseHealth = 50f;
+	[SerializeField] private float _baseMana = 20f;
+	[SerializeField] private float _baseDamage = 10f;
+	[SerializeField] private float _baseAttackSpeed = 1.5f;
+	[SerializeField] private float _baseHealthRegen = 1f;
+	[SerializeField] private float _baseManaRegen = 4f;
+	[SerializeField] private float _baseMoveSpeed = 3f;
+	[SerializeField] private float _baseExpMultiplier = 1f;
 
 	[Header("소환할 오브젝트")]
 	[SerializeField] GameObject _prefab;
@@ -28,14 +28,14 @@ public class CPlayerDataSO : ScriptableObject
 	public string Description => _description;
 	public Sprite CharacterPortrait => _characterPortrait;
 
-	public float MaxHealth => _maxHealth;
-	public float MaxMana => _maxMana;
-	public float DefaultDamage => _defaultDamage;
-	public float AttackSpeed => _attackSpeed;
-	public float HealthRegen => _healthRegen;
-	public float ManaRegen => _manaRegen;
-	public float MoveSpeed => _moveSpeed;
-	public float ExpMultiplier => _expMultiplier;
+	public float BaseHealth => _baseHealth;
+	public float BaseMana => _baseMana;
+	public float BaseDamage => _baseDamage;
+	public float BaseAttackSpeed => _baseAttackSpeed;
+	public float BaseHealthRegen => _baseHealthRegen;
+	public float BaseManaRegen => _baseManaRegen;
+	public float BaseMoveSpeed => _baseMoveSpeed;
+	public float BaseExpMultiplier => _baseExpMultiplier;
 
 	public GameObject Prefab => _prefab;
 	#endregion
