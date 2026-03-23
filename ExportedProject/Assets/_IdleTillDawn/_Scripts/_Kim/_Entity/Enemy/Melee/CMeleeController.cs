58bfe0467f0880d1402f6b73c2f04937317e5773
JiRungYeeOwO <hosik9398@gmail.com>
2026-03-23 17:16:37 +0900

    기능 수정 : 근접 몬스터 넉백
    넉백 레이어 추가, 에너미 베이스 불 타입 넉백 프로퍼티 추가,

--- ExportedProject/Assets/_IdleTillDawn/_Scripts/_Kim/_Entity/Enemy/Melee/CMeleeController.cs
+++ ExportedProject/Assets/_IdleTillDawn/_Scripts/_Kim/_Entity/Enemy/Melee/CMeleeController.cs
@@ -4,24 +4,41 @@ using UnityEngine;
 
 public class CMeleeController : CEnemyBase
 {
-    //protected override void Start()
-    //{
-    //    base.Start();
+    #region 인스펙터
+    [Header("넉백 설정")]
+    [SerializeField] private float _knockBackForce = 5f;
+    [SerializeField] private float _knockBackTime = 0.2f;
+    [SerializeField] private string _knockBackLayer = "Enemy_KnockBack";
+    #endregion
 
-    //    CPlayerController player = FindFirstObjectByType<CPlayerController>();
+    #region 내부 변수
+    private WaitForSeconds _knockBackWait;
+    #endregion
 
-    //    if (player != null)
-    //    {
-    //        SetTarget(player.transform);
-    //        Debug.Log($"타겟 : {player.name}");
-    //    }
-    //    else
-    //    {
-    //        Debug.LogWarning($"{gameObject.name} : 플레이어를 찾을 수 없음");
-    //    }
+    protected override void Awake()
+    {
+        base.Awake();
+        _knockBackWait = new WaitForSeconds(_knockBackTime);
+    }
 
-    //    InitEnemy(1);
-    //}
+    protected override void Start()
+    {
+        base.Start();
+
+        CPlayerController player = FindFirstObjectByType<CPlayerController>();
+
+        if (player != null)
+        {
+            SetTarget(player.transform);
+            Debug.Log($"타겟 : {player.name}");
+        }
+        else
+        {
+            Debug.LogWarning($"{gameObject.name} : 플레이어를 찾을 수 없음");
+        }
+
+        InitEnemy(1);
+    }
 
     protected override void ExecuteAttack()
     {
@@ -31,5 +48,30 @@ public class CMeleeController : CEnemyBase
         {
             TargetDamageable.TakeDamage(AttackDamage);
         }
+
+        StartCoroutine(CoAttackKnockBack());
+    }
+
+    /// <summary>
+    /// 공격 시 뒤로 밀려나는 코루틴 함수
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator CoAttackKnockBack()
+    {
+        IsKnockBacked = true;
+        Rb.velocity = Vector2.zero;
+
+        int originLayer = gameObject.layer;
+        gameObject.layer = LayerMask.NameToLayer(_knockBackLayer);
+
+        Vector2 knockBackDir = (transform.position - CurrentTarget.position).normalized;
+        Rb.AddForce(knockBackDir * _knockBackForce, ForceMode2D.Impulse);
+
+        yield return _knockBackWait;
+
+        gameObject.layer = originLayer;
+
+        Rb.velocity = Vector2.zero;
+        IsKnockBacked = false;
     }
 }
