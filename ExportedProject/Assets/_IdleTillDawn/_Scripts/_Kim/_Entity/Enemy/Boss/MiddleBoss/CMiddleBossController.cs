353fcd22671776c49a791f7b63a37d597c2665b4
JiRungYeeOwO <hosik9398@gmail.com>
2026-03-25 18:13:48 +0900

    기능 추가 : 원거리 몬스터
    원거리 몬스터 애니메이션 및 애니메이터 추가, 원거리 몬스터 프리팹 및 스크립트 추가, 월드 1 보스 이동 및 애니메이션 일부 구현

---
+++ ExportedProject/Assets/_IdleTillDawn/_Scripts/_Kim/_Entity/Enemy/Boss/MiddleBoss/CMiddleBossController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CMiddleBossController : CBossBase
+{
+    #region 인스펙터
+    [Header("중간 보스 패턴 설정")]
+    [SerializeField] private float _dashForce = 10f;
+    [SerializeField] private float _prepareTime = 1.5f;
+    [SerializeField] private float _slidingTime = 0.8f;
+    [SerializeField] private string _dashLayer = "Dash_Layer";
+    #endregion
+
+    protected override IEnumerator CoAttackSequence()
+    {
+        int originLayer = gameObject.layer;
+        gameObject.layer = LayerMask.NameToLayer(_dashLayer);
+
+        yield return base.CoAttackSequence();
+
+        gameObject.layer = originLayer;
+    }
+
+    protected override IEnumerator CoTelegraph()
+    {
+        Rb.velocity = Vector2.zero;
+
+        Debug.Log($"{gameObject.name} 돌진 준비");
+
+        yield return new WaitForSeconds(_prepareTime);
+    }
+
+    protected override IEnumerator CoProcessPattern()
+    {
+        if (CurrentTarget == null) yield break;
+
+        Vector2 dashDir = (CurrentTarget.position - transform.position).normalized;
+
+        Rb.AddForce(dashDir * _dashForce, ForceMode2D.Impulse);
+
+        yield return new WaitForSeconds(_slidingTime);
+
+        Rb.velocity = Vector2.zero;
+    }
+}
---
+++ ExportedProject/Assets/_IdleTillDawn/_Scripts/_Kim/_Entity/Enemy/Common/CRangedController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CRangedController : CEnemyBase
+{
+    #region 인스펙터
+    [Header("참조")]
+    [SerializeField] private GameObject _projectilePrefab;
+    [SerializeField] private Transform _firePoint;
+    #endregion
+
+    protected override void Start()
+    {
+        base.Start();
+
+        CPlayerController player = FindFirstObjectByType<CPlayerController>();
+
+        if (player != null)
+        {
+            SetTarget(player.transform);
+            Debug.Log($"타겟 : {player.name}");
+        }
+        else
+        {
+            Debug.LogWarning($"{gameObject.name} : 플레이어를 찾을 수 없음");
+        }
+
+        InitEnemy(1);
+    }
+
+    protected override void ExecuteAttack()
+    {
+        if (_projectilePrefab == null || CurrentTarget == null) return;
+
+        Vector2 dir = (CurrentTarget.position - transform.position).normalized;
+
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        GameObject obj = Instantiate(_projectilePrefab, _firePoint.position, rotation);
+
+        CProjectileTest projectile = obj.GetComponent<CProjectileTest>();
+
+        if (projectile != null)
+        {
+            projectile.Init(AttackDamage, dir);
+        }
+    }
+}
