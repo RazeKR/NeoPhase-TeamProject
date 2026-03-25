72a99f69661f08ce94ecb18b122a53c7b497914d
JiRungYeeOwO <hosik9398@gmail.com>
2026-03-25 18:13:48 +0900

    기능 추가 : 원거리 몬스터
    원거리 몬스터 애니메이션 및 애니메이터 추가, 원거리 몬스터 프리팹 및 스크립트 추가, 월드 1 보스 이동 및 애니메이션 일부 구현

---
+++ ExportedProject/Assets/_IdleTillDawn/_Scripts/_Kim/_Entity/Enemy/Boss/World1/CWorldBossType1Controller.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CWorldBossType1Controller : CBossBase
+{
+    #region 인스펙터
+    [Header("패턴 설정")]
+    [SerializeField] private float _dashForce = 10f;
+    [SerializeField] private float _slidingTime = 1.5f;
+    [SerializeField] private string _dashLayer = "Dash_Layer";
+    #endregion
+
+    protected override IEnumerator CoAttackSequence()
+    {
+        int originLayer = gameObject.layer;
+        int dashLayer = LayerMask.NameToLayer(_dashLayer);
+
+        SetLayerRecursively(gameObject, dashLayer);
+
+        yield return base.CoAttackSequence();
+
+        SetLayerRecursively(gameObject, originLayer);
+    }
+
+    protected override IEnumerator CoTelegraph()
+    {
+        Rb.velocity = Vector2.zero;
+
+        Debug.Log($"{gameObject.name} 돌진 준비");
+
+        yield return new WaitForSeconds(0.2f);
+    }
+
+    protected override IEnumerator CoProcessPattern()
+    {
+        if (CurrentTarget == null) yield break;
+
+        Vector2 dashDir = (CurrentTarget.position - transform.position).normalized;
+
+        Rb.AddForce(dashDir * _dashForce, ForceMode2D.Impulse);
+
+        yield return new WaitForSeconds(_slidingTime);
+
+        Rb.velocity = Vector2.zero;
+    }
+
+    /// <summary>
+    /// 자신을 포함한 모든 자식의 레이어를 바꾸는 재귀함수
+    /// </summary>
+    /// <param name="go"></param>
+    /// <param name="newLayer"></param>
+    private void SetLayerRecursively(GameObject go, int newLayer)
+    {
+        go.layer = newLayer;
+
+        foreach (Transform child in go.transform)
+        {
+            SetLayerRecursively(child.gameObject, newLayer);
+        }
+    }
+}
---
+++ ExportedProject/Assets/_IdleTillDawn/_TestFiles/CProjectileTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CProjectileTest : MonoBehaviour
+{
+    #region 인스펙터
+    [Header("투사체 속도")]
+    [SerializeField] private float _speed = 8f;
+    #endregion
+
+    #region 내부 변수
+    private float _damage;
+    private Vector2 _direction;
+    #endregion
+
+    public void Init(float damage, Vector2 direction)
+    {
+        _damage = damage;
+        _direction = direction;
+
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.velocity = _direction * _speed;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        IDamageable target = collision.GetComponent<IDamageable>();
+        if (target != null)
+        {
+            target.TakeDamage(_damage);
+            Destroy(gameObject);
+        }
+    }
+}
