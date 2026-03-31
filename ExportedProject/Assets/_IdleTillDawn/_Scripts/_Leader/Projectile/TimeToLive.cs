388f4ccb33d5456446643e2d4054dd10f665d6bd
Raze <tlgus3305@gmail.com>
2026-03-31 18:01:48 +0900

    총알 투사체 발사간격 버그수정
    플레이어 무기 발사 테스트 기능 및 발사간격 버그 수정

--- ExportedProject/Assets/_IdleTillDawn/_Scripts/_Leader/Projectile/TimeToLive.cs
+++ ExportedProject/Assets/_IdleTillDawn/_Scripts/_Leader/Projectile/TimeToLive.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 namespace flanne
 {
@@ -10,15 +10,25 @@ namespace flanne
         [SerializeField]
         private bool willDestroy;
 
+        /// <summary>
+        /// CWeaponDataSO.LifeTime 등 외부에서 수명을 주입할 때 사용합니다.
+        /// </summary>
+        public void SetLifetime(float newLifetime)
+        {
+            lifetime = newLifetime;
+            CancelInvoke();
+            Invoke(nameof(Deactivate), lifetime);
+        }
+
         public void Refresh()
         {
             CancelInvoke();
-            Invoke("Deactivate", lifetime);
+            Invoke(nameof(Deactivate), lifetime);
         }
 
         private void OnEnable()
         {
-            Invoke("Deactivate", lifetime);
+            Invoke(nameof(Deactivate), lifetime);
         }
 
         private void Deactivate()
@@ -29,7 +39,7 @@ namespace flanne
             }
             else
             {
-                base.gameObject.SetActive(value: false);
+                base.gameObject.SetActive(false);
             }
         }
 
